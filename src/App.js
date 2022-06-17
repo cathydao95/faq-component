724b97ae86e8244f6de7bc46259cadc4efbad306
Cathy Dao <cathydao95@gmail.com>
2022-06-17 10:24:58 -0700

    add question object and map through object

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Question from "./components/Question";
+import { useState } from "react";
 
 function App() {
+  const questionObjects = [
+    {
+      question: "How many bones does a cat have?",
+      answer: "A cat has 230 bones - 6 more than a human",
+    },
+    {
+      question: "How much do cats sleep?",
+      answer: "The average cat sleeps 12-16 hours per day",
+    },
+    {
+      question: "How long do cats live",
+      answer:
+        "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Frequently Asked Questions</h1>
+      {questionObjects.map((item) => {
+        return (
+          <Question
+            question={item.question}
+            answer={item.answer}
+            key={item.question}
+          />
+        );
+      })}
     </div>
   );
 }
